63bf4ea02da0375920dc6c94123c019c8ca187a4
unknown <tiaraamandapyb@gmail.com>
2023-12-20 13:00:00 +0700

    first commit

---
+++ src/sif1pagi/fikti.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sif1pagi;
+
+import LoginPackage.JFrameLogin;
+
+public class fikti {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      JFrameLogin objLog = new JFrameLogin();
+      objLog.show();
+    }
+    
+}
